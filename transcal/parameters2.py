1fdae232a926f3af39b14c5efcadab98ae11c3d9
gabrielcmoran <gabriel.c.moran20@gmail.com>
2024-06-06 21:20:18 -0300

    começando dnv

--- transcal/parameters2.py
+++ transcal/parameters2.py
@@ -3,6 +3,7 @@ import math
 # dimensions
 l_a = 5*(10**-3) # [m]
 l_b = 10*(10**-3) # [m]
+l_c = 5*(10**-3) # [m]
 h_a = 8*(10**-3) # [m]
 h_b = 8*(10**-3) # [m]
 h_c = 4*(10**-3) # [m]
@@ -16,11 +17,12 @@ Ti = 15 # initial temperature [°C]
 
 # discretization
 delta_t = 1 # [s]
-M = 3
-N = 3
+M = 22
+N = 22
 
-M_a = math.floor(M/2)
-M_b = M_a + math.floor(M/2) + M%2
+M_a = math.floor(M/3)
+M_b = M_a + math.floor(M/3)
+M_c = M_b + math.floor(M/3)+ M%3
 
 N_a = math.floor(N/3)
 N_b = N_a + math.floor(N/3)
@@ -28,6 +30,7 @@ N_c = N_b + math.floor(N/3)+ N%3
 
 delta_x_a = l_a/M_a
 delta_x_b = l_b/(M_b - M_a)
+delta_x_c = l_c/(M_c - M_b)
 
 delta_y_a = h_a/N_a
 delta_y_b = h_b/(N_b - N_a)
