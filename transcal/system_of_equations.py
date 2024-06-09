39204b20c0f1820606c77b965a76a3f7f1b4a4ce
gabrielcmoran <gabriel.c.moran20@gmail.com>
2024-06-09 01:20:20 -0300

    progressing

--- transcal/system_of_equations.py
+++ transcal/system_of_equations.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def resolving_system_of_equations(T,knot,M,N):
     A = np.zeros((M*N,M*N))
@@ -9,11 +10,6 @@ def resolving_system_of_equations(T,knot,M,N):
     for m in range(M):
         for n in range(N):
             k = m * N + n
-            #A[k,k]		coef do nó do momento
-            #A[k,k-1]		coef do nó da esq
-            #A[k,k+1]		coef do nó da dir
-            #A[k,k+(n+1)]		coef nó de baixo
-            #A[k,k-(n+1)]		coef nó de cima
 
             if knot.group == 1.1:
                 A[k][k] = 1
@@ -21,11 +17,177 @@ def resolving_system_of_equations(T,knot,M,N):
                 A[k][k-(N+1)] = 3
                 b[k] = 4
             
-            if knot.group == 1.2:
+            elif knot.group == 1.2:
                 A[k][k] = 1
                 A[k][k+1] = 2
                 A[k][k+(N+1)] = 3
                 b[k] = 4
+            
+            elif knot.group == 1.3:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 1.4:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.11:
+                A[k][k] = 1
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.12:
+                A[k][k] = 1
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.13:
+                A[k][k] = 1
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.14:
+                A[k][k] = 1
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.15:
+                A[k][k] = 1
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.21:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.22:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.31:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.32:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.33:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.41:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.42:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 2.43:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 3.1:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k-(N+1)] = 3
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 3.2:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k-(N+1)] = 3
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 3.3:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 3.4:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 4.1:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 4.2:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                b[k] = 4
+            
+            elif knot.group == 4.3:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+
+            elif knot.group == 5:
+                A[k][k] = 1
+                b[k] = 2
+            
+            elif math.floor(knot.group) == 6:
+                A[k][k] = 1
+                A[k][k-1] = 2
+                A[k][k+1] = 2
+                A[k][k+(N+1)] = 3
+                A[k][k-(N+1)] = 3
+                b[k] = 4
+                
+            
 
 
     b_new = np.linalg.solve(A, b)
